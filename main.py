763cd4c8840465662bf5e048254b3698eced7925
Muhammad Taha Hasnat <141823914+tahahasnat018@users.noreply.github.com>
2025-08-13 20:18:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -158,9 +158,10 @@ async def media_ws(websocket: WebSocket):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
-        "app:app",
+        "main:app",
         host="0.0.0.0",
         port=8000,
         reload=True
     )
+
 
